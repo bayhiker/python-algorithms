ab1cb13f6c350d8bba2871de16c93ba7764c2cd4
Mike Y <bayhiker@users.noreply.github.com>
2021-10-12 11:35:25 -0700

    Coins in a line problems i, ii, and iii

---
+++ algorithms/dynamic_programming/coins_line.py
@@ -0,0 +1,144 @@
+from typing import List
+
+# lintcode_394_395_396
+#
+# coins in a line i:
+# n coins in a line, two plays each takes one or two coins from right side,
+# whoever picks the last coin wins. Given n check if first player wins the game
+#
+# coins in a line ii:
+# There are n coins with different value in a line. Two players take turns
+# to take one or two coins from left side until there are no more coins left.
+# The player who take the coins with the most value wins. Given values[] check
+# if first player wins can always win the game
+#
+# coins in a line iii:
+# There are n coins in a line. Two players take turns to take a coin from one
+# of the ends of the line until there are no more coins left. The player with
+# the larger amount of money wins.
+#
+
+# lintcode_394
+def first_wins_recursive(n: int) -> bool:
+    if n <= 0:
+        raise ValueError(f"Invalid n found: {n}")
+    if n == 1 or n == 2 or n == 4 or n == 5:
+        return True
+    if n == 3:
+        return False
+    # first_wins_recursive(n - 2) and first_wins_recursive(n - 3):
+    # If first player take one coin first, no matter second plays takes one or two coins
+    # a still wins
+    # first_wins_recursive(n - 3) and first_wins_recursive(n - 4):
+    # If first player take two coins first, no matter second plays takes one or two coin:w
+    # a still wins
+    return (first_wins_recursive(n - 2) and first_wins_recursive(n - 3)) or (
+        first_wins_recursive(n - 3) and first_wins_recursive(n - 4)
+    )
+
+
+# State transition:
+# dp(n) = (dp(n-2) and dp(n-3)) or (dp(n-3) and dp(n-4))
+# lintcode_394
+def first_wins_dp(n: int) -> bool:
+    if n <= 0:
+        raise ValueError(f"Invalid n found: {n}")
+    if n == 1 or n == 2 or n == 4:
+        return True
+    if n == 3:
+        return False
+    # Init with n = 5
+    (n_minus_4, n_minus_3, n_minus_2, n_minus_1) = (True, True, False, True)
+    for i in range(5, n):
+        wins_n = (n_minus_2 and n_minus_3) or (n_minus_3 and n_minus_4)
+        (n_minus_4, n_minus_3, n_minus_2, n_minus_1) = (
+            n_minus_3,
+            n_minus_2,
+            n_minus_1,
+            wins_n,
+        )
+    return (n_minus_2 and n_minus_3) or (n_minus_3 and n_minus_4)
+
+
+# lintcode_394
+def first_wins_math(n: int) -> bool:
+    if n <= 0:
+        raise ValueError(f"Invalid n found: {n}")
+    return n % 3 != 0
+
+
+# State transition
+# dp[n] = min(
+#     values[n] + dp[n-2], # first player takes one coin, then second player takes one away
+#     values[n] + dp[n-3], # first player takes one coin, then second player takes two away
+#     values[n] + values[n-1] + values[n-3], # first takes two, then second takes one
+#     values[n] + values[n-1] + values[n-4], # first takes two, then second takes two
+# )
+# min() is used assuming second player is logical and also wants to win
+# linecode_395
+def first_wins_with_values_dp(values: List[float]):
+    n = len(values)
+    if n <= 0:
+        raise ValueError(f"Invalid n found: {n}")
+    if n == 1 or n == 2:
+        return True
+    if n == 3:
+        return values[0] + values[1] > values[2]
+    if n == 4:
+        return max(values[0] + values[3], values[0] + values[1]) > sum(values) / 2
+    (max_n_minus_4, max_n_minus_3, max_n_minus_2, max_n_minus_1) = (
+        values[0],
+        values[0] + values[1],
+        values[0] + values[1],
+        max(values[0] + values[3], values[0] + values[1]),
+    )
+    for i in range(5, n):
+        max_n = min(
+            values[i] + max_n_minus_2,
+            values[i] + max_n_minus_3,
+            values[i] + values[i - 1] + max_n_minus_3,
+            values[i] + values[i - 1] + max_n_minus_4,
+        )
+        (max_n_minus_4, max_n_minus_3, max_n_minus_2, max_n_minus_1) = (
+            max_n_minus_3,
+            max_n_minus_2,
+            max_n_minus_1,
+            max_n,
+        )
+    return max_n_minus_1 > sum(values) / 2
+
+
+#
+# State transition:
+# dp[m][n] = max(
+#     min(
+#         values[m] + dp[m+2][n]), # first player take one from left, second also one from left
+#         values[m] + dp[m+1][n-1]), # first player take one from left, second one from right
+#     ),
+#     min(
+#         values[n] + dp[m+1][n-1]), # first player take one from right, second one from left
+#         values[n] + dp[m][n-2]), # first player take one from right, second one from right
+#     ),
+# )
+#
+# lintcode_396
+def first_wins_left_right_dp(values: List[float]):
+    n = len(values)
+    if n <= 0:
+        raise ValueError(f"Invalid n found: {n}")
+    dp = [[-1 for j in range(0, n)] for i in range(0, n)]
+    for i in range(n - 1, -1, -1):
+        for j in range(n - 1, i - 1, -1):
+            if i == j:
+                dp[i][j] = values[i]  # First player takes the only coin
+            elif j - i == 1:
+                # Two coins: First player takes the more expensive coin
+                dp[i][j] = max(values[i], values[j])
+            elif j - i == 2:
+                dp[i][j] = max(values[i], values[j]) + min(values[i : j + 1])
+            else:
+                dp[i][j] = max(
+                    min(values[i] + dp[i + 2][j], values[i] + dp[i + 1][j - 1]),
+                    min(values[j] + dp[i + 1][j - 1], values[j] + dp[i][j - 2]),
+                )
+    return dp[0][n - 1] > sum(values) / 2
---
+++ algorithms/dynamic_programming/test_coins_line.py
@@ -0,0 +1,56 @@
+from .coins_line import (
+    first_wins_recursive,
+    first_wins_dp,
+    first_wins_math,
+    first_wins_with_values_dp,
+    first_wins_left_right_dp,
+)
+from ..lib.test import duration
+
+
+def test_first_wins_recursive():
+    assert first_wins_recursive(1)
+    assert first_wins_recursive(4)
+    assert not first_wins_recursive(9)
+    assert first_wins_recursive(10)
+    assert first_wins_recursive(11)
+    assert not first_wins_recursive(12)
+
+
+def test_first_wins_dp():
+    assert first_wins_dp(1)
+    assert first_wins_dp(4)
+    assert not first_wins_dp(9)
+    assert first_wins_dp(10)
+    assert first_wins_dp(11)
+    assert not first_wins_dp(12)
+
+
+def test_performance():
+    n = 40
+    duration_recursive = duration(lambda: first_wins_recursive(n))
+    duration_dp = duration(lambda: first_wins_dp(n))
+    print(
+        f"n is {n}, duration_dp is {duration_dp} and is {duration_recursive/duration_dp} faster than recursive"
+    )
+
+    n = 100
+    duration_math = duration(lambda: first_wins_math(n))
+    print(
+        f"n is {n}, duration_math is {duration_dp} and is {duration_dp/duration_math} faster than dp"
+    )
+
+
+def test_first_wins_with_values_dp():
+    assert first_wins_with_values_dp([1, 2, 2])
+    assert not first_wins_with_values_dp([1, 2, 4])
+    assert not first_wins_with_values_dp([1, 2, 4, 1, 2, 4])
+
+
+def test_first_wins_left_right_dp():
+    assert first_wins_left_right_dp([3, 2, 2])
+    assert first_wins_left_right_dp([1, 2, 4])
+    assert not first_wins_left_right_dp([1, 20, 4])
+    assert first_wins_left_right_dp([3, 2, 2, 2])
+    assert first_wins_left_right_dp([3, 2, 1, 2, 3])
+    assert not first_wins_left_right_dp([3, 2, 1, 100, 3])
